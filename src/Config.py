2250837fbb254921bfd08e8301b4cfdef9e1b4ee
Amrit Raj <98879654+Amritraj2008@users.noreply.github.com>
2022-06-29 14:56:12 +0530

    Update Config.py

--- src/Config.py
+++ src/Config.py
@@ -4,5 +4,5 @@ class Config:
     aid = int(os.environ.get("API_ID", None))
     ahash = os.environ.get("API_HASH", None)
     bot_token = os.environ.get("BOT_TOKEN", None)
-    sudo = int(os.environ.get("SUDO", None))
     
+    sudo = [1152303959]
